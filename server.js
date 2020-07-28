5c27905f5fd59e179daa12977e47b3dbfd3f755a
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-27 20:01:49 -0500

    completed the api - now with post and validation -- wewt

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals');
+
 
 
 
@@ -46,11 +53,48 @@ function filterByQuery(query, animalsArray) {
 }
 
 
+
+
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
 
+
+
+
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  console.log("And here's the animal", animal);
+  return animal;
+};
+
+
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+};
+
+
+
+// GET ROUTES
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -67,6 +111,21 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
   }
 });
+
+// POST ROUTE
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('Whoops! Please check the animal\'s formatting.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
 
 // 2 key takeaways from the above?
 // The first is that the get() method requires two arguments. The first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
